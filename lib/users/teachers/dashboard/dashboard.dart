1d95709711c7cd9b66cb06cbc822e31cda449a23
Aditya Singh <aditya.singh9868@gmail.com>
2021-05-24 16:30:26 +0530

    Teachers Dashboard

---
+++ lib/users/teachers/dashboard/dashboard.dart
@@ -0,0 +1,42 @@
+import 'package:attend/index.dart';
+
+class TeacherDashboard extends StatefulWidget {
+  TeacherDashboard({Key key}) : super(key: key);
+
+  @override
+  _TeacherDashboardState createState() => _TeacherDashboardState();
+}
+
+class _TeacherDashboardState extends State<TeacherDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      appBar: CustomAppBar(
+        text: "TeacherDashboard",
+        showPrefix: false,
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      isScrollable: true,
+      children: [
+        DashboardCard(
+          subtitle: "QR Code",
+          title: " Scanner",
+          colors: AppColor.card1,
+          icon: Icons.qr_code_scanner_outlined,
+          onPressed: () async {
+            CommonWidgets.push(context, QrScreen());
+          },
+        ),
+        DashboardCard(
+          subtitle: "View",
+          title: "Attendance",
+          colors: AppColor.card2,
+          icon: Icons.grading_outlined,
+          onPressed: () async {
+            CommonWidgets.push(context, ViewAttendanceStudent());
+          },
+        ),
+      ],
+    );
+  }
+}
